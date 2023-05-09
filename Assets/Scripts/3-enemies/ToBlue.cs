813207e28e37c132402c34b37648712113fa5061
Dvir-Segal <Dvirsegal96@gmail.com>
2023-05-10 01:11:58 +0300

    cygvjhb

---
+++ Assets/Scripts/3-enemies/ToBlue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/**
+ * This component just rotates its object between angular bounds.
+ */
+public class ToBlue : MonoBehaviour {
+
+
+    void Update() {
+            GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
---
+++ Assets/Scripts/3-enemies/ToRed.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/**
+ * This component just rotates its object between angular bounds.
+ */
+public class ToRed : MonoBehaviour {
+
+
+    void Update() {
+            GetComponent<Renderer>().material.color = Color.red;
+    }
+}
